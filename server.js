ea79be9e82396ed3a96f8f65dccef5ffa74ade58
vascuk <vascuk.r@nemk.ukr.education>
2026-03-17 20:59:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const PORT = 3000;
 
 // Підключення до MySQL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME
 });
 
 db.connect(err => {
